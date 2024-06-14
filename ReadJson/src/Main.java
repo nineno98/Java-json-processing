3eaabc89c6294e1cad1a86c65988c3219131e6b1
nineno98 <marcell.kovacs123@gmail.com>
2024-06-14 15:19:58 +0000

    json file

--- ReadJson/src/Main.java
+++ ReadJson/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.json.simple.
+
 public class Main {
+    public static List<Dolgozo> dolgozok = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        beolvasas();
+
+    }
+
+    private static void beolvasas() {
+        Object obj = new JSONParser.parse
     }
 }
